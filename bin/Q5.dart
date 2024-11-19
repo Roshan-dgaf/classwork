88219b77bc59b9062df06e38e61b8aa2d3f0dbd4
Roshan Baidar <roshanbaidar1234@gmail.com>
2024-11-19 22:28:05 +0545

    Q5

---
+++ bin/Q5.dart
@@ -0,0 +1,10 @@
+void main() {
+  int n = 10; // Replace with the desired value of n
+  int sum = 0;
+
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  print('The sum of natural numbers up to $n is $sum.');
+}
