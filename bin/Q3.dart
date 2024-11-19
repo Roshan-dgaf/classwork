e72658ccca16684c68baee485e1759d75f7303c2
Roshan Baidar <roshanbaidar1234@gmail.com>
2024-11-19 22:25:32 +0545

    Q3

---
+++ bin/Q3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = -5; // Change this value to test different numbers
+
+  if (number > 0) {
+    print('$number is positive.');
+  } else if (number < 0) {
+    print('$number is negative.');
+  } else {
+    print('$number is zero.');
+  }
+}
